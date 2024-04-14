0cdeca83ca72dffda840ec7d5c342447c2031786
weiss <kingbaiing@163.com>
2024-04-14 09:08:30 +0200

    use crane instead of cargo2nix

--- flake.nix
+++ flake.nix
@@ -1,70 +1,85 @@
 {
   inputs = {
-    cargo2nix.url = "github:cargo2nix/cargo2nix/unstable";
-    flake-utils.follows = "cargo2nix/flake-utils";
-    nixpkgs.follows = "cargo2nix/nixpkgs";
+    # cargo2nix.url = "github:cargo2nix/cargo2nix/unstable";
+    # nixpkgs.follows = "cargo2nix/nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    crane.url = "github:ipetkov/crane";
+    crane.inputs.nixpkgs.follows = "nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
     devshell.url = "github:numtide/devshell";
     rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = inputs:
-    with inputs;
+  outputs = { self, crane, flake-utils, devshell, nixpkgs, rust-overlay }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
           overlays = [
-            cargo2nix.overlays.default
+            # cargo2nix.overlays.default
             devshell.overlays.default
             (import rust-overlay)
           ];
         };
-
-        rustPkgs = pkgs.rustBuilder.makePackageSet {
-          rustVersion = "1.66.1";
-          packageFun = import ./Cargo.nix;
-          # Use the existing all list of overrides and append your override
-          packageOverrides = pkgs:
-            pkgs.rustBuilder.overrides.all ++ [
-              (pkgs.rustBuilder.rustLib.makeOverride {
-                name = "sqlx-macros";
-                overrideAttrs = drv: {
-                  propagatedBuildInputs = with pkgs;
-                    drv.propagatedBuildInputs or [ ]
-                    ++ (lib.optional stdenv.isDarwin
-                      (with darwin.apple_sdk.frameworks;
-                        [ SystemConfiguration ]));
-                };
-              })
-            ];
-        };
-
-      in with pkgs; rec {
+        extraBuildInputs = with pkgs;
+          if stdenv.isDarwin then
+            with darwin.apple_sdk.frameworks; [
+              IOKit
+              Security
+              CoreServices
+              SystemConfiguration
+            ]
+          else [
+            gcc
+            openssl
+            libiconv
+            pkg-config
+          ];
+      in {
         devShells.default = pkgs.devshell.mkShell {
           imports = map pkgs.devshell.importTOML [ ./devshell.toml ];
           packages = with pkgs;
-            [ rust-bin.stable."1.66.1".default ] ++ (if stdenv.isDarwin then
-              with darwin.apple_sdk.frameworks; [
-                IOKit
-                Security
-                CoreServices
-                SystemConfiguration
-              ]
-            else [
-              gcc
-              openssl
-              libiconv
-              pkg-config
-            ]);
+            [ rust-bin.stable."1.66.1".default ] ++ extraBuildInputs;
           env = [{
             name = "PKG_CONFIG_PATH";
             value = "${pkgs.openssl.dev}/lib/pkgconfig";
           }];
         };
-        packages = {
-          webman-cli = (rustPkgs.workspace.webman-cli { }).bin;
-          webman-server = (rustPkgs.workspace.webman-server { }).bin;
-          default = packages.webman-cli;
+        packages = let
+          craneLib = crane.lib.${system};
+          sqlxFilter = path: _type:
+            null != builtins.match ".*sql$" path || null
+            != builtins.match ".*json$" path || null
+            != builtins.match ".*env$" path;
+
+          common = {
+            version = "0.1";
+            # src = craneLib.path ./.;
+            src = pkgs.lib.cleanSourceWith {
+              src = craneLib.path ./.;
+              filter = path: type:
+                (sqlxFilter path type)
+                || (craneLib.filterCargoSources path type);
+            };
+            strictDeps = true;
+            buildInputs = extraBuildInputs;
+            nativeBuildInputs = with pkgs; [ pkg-config ];
+            PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+            doCheck = false;
+          };
+          cargoArtifacts = craneLib.buildDepsOnly common;
+        in rec {
+          webman-cli = craneLib.buildPackage (common // {
+            pname = "webman-cli";
+            cargoExtraArgs = "-p webman-cli";
+          });
+
+          webman-server = craneLib.buildPackage (common // {
+            pname = "webman-server";
+            cargoExtraArgs = "-p webman-server";
+          });
+
+          default = webman-cli;
         };
       });
 }
